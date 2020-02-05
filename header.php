661e1e9d83c0d83207a60e6030963c3b8addac79
Ryan Pearson <ryanpinpb@gmail.com>
2020-02-05 14:30:10 -0800

    adjusted hamburger icon to display flex instead of using top and left to position it

--- header.php
+++ header.php
@@ -35,56 +35,49 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'pacificbeachhomes' ); ?></a>
+
+		<header id="masthead" class="site-header header is-visible">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				?>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<!-- laptop and desktop menu -->
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+
+				<!-- mobile menu with hamburger  -->
+				<div class="mobileMenu">
+					<?php
+					wp_nav_menu( array(
+						'menu'				=> 'Mobile',
+						'container_class'	=> 'mobileMenu-content'
+					) );
+					?>
+				</div>
+				<div class="menu-icon">
+					<div class="hamburgerContainer">
+						<div class="hamburger"></div>
+					</div>
+					<div class="hamburgertext">MENU</div>
+				</div>
+
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
+		
+		
 		<?php if ( is_front_page() ) : ?>
 			<section id="banner" class="frontBanner">
 		<?php else: ?>
 			<section id="banner" class="internal">
 		<?php endif; ?>
-			<header id="masthead" class="site-header header is-visible">
-				<div class="site-branding">
-					<?php
-					the_custom_logo();
-					if ( is_front_page() && is_home() ) :
-						?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php
-					else :
-						?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php
-					endif;
-					$pacificbeachhomes_description = get_bloginfo( 'description', 'display' );
-					if ( $pacificbeachhomes_description || is_customize_preview() ) :
-						?>
-						<p class="site-description"><?php echo $pacificbeachhomes_description; /* WPCS: xss ok. */ ?></p>
-					<?php endif; ?>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation">
-					<!-- laptop and desktop menu -->
-					<?php
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					) );
-					?>
-
-					<!-- mobile menu with hamburger  -->
-					<div class="mobileMenu">
-						<?php
-						wp_nav_menu( array(
-							'menu'				=> 'Mobile',
-							'container_class'	=> 'mobileMenu-content'
-						) );
-						?>
-					</div>
-					<div class="menu-icon">
-						<div class="hamburger"></div>
-						<div class="hamburgertext">MENU</div>
-					</div>
-
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
+			
 		<?php if ( is_front_page() ) : ?>
 			<span class="banner-text">Welcome to Pacific Beach</span>
 		<?php else:  ?>
