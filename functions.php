37a9882694fedabd35315b48619a81df0f39c30e
Ryan Pearson <ryanpinpb@gmail.com>
2020-05-02 19:28:07 -0700

    routed all WP css through app.scss and deferred stylesheet to footer

--- functions.php
+++ functions.php
@@ -137,27 +137,18 @@ add_action( 'widgets_init', 'pacificbeachhomes_widgets_init' );
  * Enqueue scripts and styles.
  */
 function pacificbeachhomes_scripts() {
-	
-	// wp_enqueue_style( 'pacificbeachhomes-style', get_stylesheet_uri() );
-	
-	if ( $_SERVER['SERVER_NAME'] === 'localhost' || strpos($_SERVER['SERVER_NAME'], '.local') !== false ) :
-		$path = '//127.0.0.1:3000';
-	else :
-		$path = get_stylesheet_directory_uri();
-		wp_enqueue_style( 'custom-style', $path . '/assets/css/main.bundle.css');
-	endif;
-
-	// wp_enqueue_style( 'owl-default-style', get_template_directory_uri() . '/assets/vendors/owl-carousel/owl.carousel.min.css' );
-	// wp_enqueue_style( 'animation', get_template_directory_uri() . '/assets/vendors/owl-carousel/animate.css' );
-	wp_enqueue_script( 'pacificbeachhomes-navigation', get_template_directory_uri() . '/assets/js/navigation.js', [], '20151215', true );
-	wp_enqueue_script( 'pacificbeachhomes-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
-	
+	// if ( $_SERVER['SERVER_NAME'] === 'localhost' || strpos($_SERVER['SERVER_NAME'], '.local') !== false ) :
+	// 	$path = '//127.0.0.1:3000';
+	// 	wp_enqueue_style( 'custom-style', $path . '/assets/css/main.bundle.css');
+	// else :
+	// 	$path = get_stylesheet_directory_uri();
+	// 	wp_enqueue_style( 'custom-style', $path . '/assets/css/main.bundle.css');
+	// endif;
+	wp_enqueue_script( 'pacificbeachhomes-navigation', get_template_directory_uri() . '/assets/js/navigation.js', [], '20151215', 'true' );
+	wp_enqueue_script( 'pacificbeachhomes-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', 'true' );
 	wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', [], null, true);
-
-	// wp_enqueue_script( 'owl-js', get_template_directory_uri() . '/assets/vendors/owl-carousel/owl.carousel.min.js', ['jquery'], null, true );
-
-	wp_enqueue_script( 'main', $path . '/assets/js/main.bundle.js', ['jquery'], null, true );
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', [], null, 'true');
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.bundle.js', ['jquery'], null, 'true' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -207,15 +198,49 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
-//preload style sheets to defer stylesheets
-function add_rel_preload($html, $handle, $href, $media) {
-    
-    if (is_admin())
-        return $html;
+//deregister plugin styles
+add_action('wp_print_styles', 'my_deregister_styles', 100);
 
-     $html = <<<EOT
-<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
-EOT;
-    return $html;
+function my_deregister_styles() {
+  wp_deregister_style('dashicons');
+  wp_deregister_style('admin-bar');
+  wp_deregister_style('frm_fonts');
+  wp_deregister_style('wp-block-library');
+  wp_deregister_style('yoast-seo-adminbar');
 }
-add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
+
+
+//enqueue style in footer instead of head
+function prefix_add_footer_styles() {
+	if ( $_SERVER['SERVER_NAME'] === 'localhost' || strpos($_SERVER['SERVER_NAME'], '.local') !== false ) :
+		$path = '//127.0.0.1:3000';
+	else :
+		$path = get_stylesheet_directory_uri();
+	endif;
+    wp_enqueue_style( 'custom-style', $path . '/assets/css/main.bundle.css');
+};
+add_action( 'get_footer', 'prefix_add_footer_styles' );
+
+//add inline styles
+function hook_css() {
+    ?>
+	<style type="text/css">a,h3{color:var(--blue)}.main-navigation a,a,a.mobile-button:hover{text-decoration:none}@font-face{font-family:Poppins;font-style:normal;font-weight:400;src:url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-regular.eot);src:local('Poppins Regular'),local('Poppins-Regular'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-regular.eot?#iefix) format('embedded-opentype'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-regular.woff2) format('woff2'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-regular.woff) format('woff'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-regular.ttf) format('truetype'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-regular.svg#Poppins) format('svg');font-display:swap}@font-face{font-family:Poppins;font-style:normal;font-weight:700;src:url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-700.eot);src:local('Poppins Bold'),local('Poppins-Bold'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-700.eot?#iefix) format('embedded-opentype'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-700.woff2) format('woff2'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-700.woff) format('woff'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-700.ttf) format('truetype'),url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/fonts/poppins-v9-latin-700.svg#Poppins) format('svg');font-display:swap}:root{--headerFont:'Poppins',sans-serif;--bodyFont:'Roboto',Helvetica;--blue:rgba(0, 174, 239,1);--blueAccent:rgba(0, 120, 163,1);--orange:rgba(240, 124, 0, 1);--white:rgba(255,255,255,1);--black:rgba(0,0,0,1)}*{margin:0;padding:0;box-sizing:border-box}html{font-size:10px}h1,h2{font-size:3.6rem}body{font-family:var(--bodyFont)}h1,h2,h3{line-height:1.2em;font-family:var(--headerFont)}.site-content{position:relative;background:var(--white);z-index:3}.clearfix::after{content:"";clear:both;display:table}h1{text-align:center;font-weight:700;margin:20px 10px;background:-webkit-linear-gradient(var(--blueAccent),var(--blue));background-clip:border-box;-webkit-background-clip:text;-webkit-text-fill-color:transparent}.mobile-text,h2,h3{font-weight:400}h2{color:var(--orange);margin:40px 10px 30px}h3{font-size:2.7rem;margin:25px 15px}p,ul{font-size:2.25rem;font-family:var(--bodyFont)}p{margin-top:20px;line-height:1.6}ul{list-style:none;margin:0 0 0 1.5em}#masthead,.mobile-text{font-family:var(--headerFont)}.screen-reader-text{border:0;clip:rect(1px,1px,1px,1px);clip-path:inset(50%);height:1px;margin:-1px;overflow:hidden;padding:0;position:absolute!important;width:1px;word-wrap:normal!important}a#scroll{display:none}#masthead{position:fixed;top:0;background:rgba(0,120,163,0);display:flex;flex-direction:row;justify-content:space-between;align-items:center;width:100%;transition:all .3s ease-in;z-index:100}#masthead.is-visible{opacity:1;transform:translate(0,0);-webkit-transform:translate(0,0);transition:transform .3s,background .3s,color .3s,opacity 0 .3s;-webkit-transition:-webkit-transform .3s,background .3s,color .3s}.nav-container{width:100%;display:flex;flex-direction:row;justify-content:space-between;align-items:center}.site-branding{display:none;align-items:center;margin:10px}.site-branding a{max-width:400px;height:auto;max-height:80px;cursor:pointer;transition:transform .3s ease-in-out}.site-branding a .custom-logo{max-width:100%;height:auto}.main-navigation{display:flex;flex-direction:row;width:100%;align-items:center;justify-content:space-evenly}.menu-main-container{display:none}a.mobile-button{display:flex;flex-direction:column;align-items:center;justify-content:center;flex:1 1 auto;height:80px;transition:opacity .8s}a.mobile-button.hidden{opacity:0}.mobile-icon-container{height:34px;margin:0}.mobile-icon{height:100%;fill:var(--white)}.mobile-text{display:flex;justify-content:center;align-items:center;color:var(--white);font-size:1.75rem}.mobileMenu,a.menu-icon{flex-direction:column;display:flex}.wrap{background-color:var(--white)}.front-banner{background-image:url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/images/backgrounds/background.jpg);background-position:center;background-size:cover;background-repeat:no-repeat;height:100vh;height:calc(var(--vho,1vh) *100);position:relative;width:100%;z-index:4}.front-banner::after{position:absolute;bottom:0;left:0;content:'';width:100%;height:10%;background:-webkit-linear-gradient(top,rgba(0,0,0,0) 0,rgba(0,0,0,.95) 80%,rgba(0,0,0,1) 100%);background:linear-gradient(to bottom,rgba(0,0,0,0) 0,rgba(0,0,0,.95) 80%,rgba(0,0,0,1) 100%)}.banner-top,.internal-banner{background-position:center;background-size:cover;background-repeat:no-repeat}.banner-top{background-image:url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/images/backgrounds/background-top.png);position:absolute;width:100%;height:100%;z-index:5}.internal-banner{background-image:url(https://pacificbeachhomes.com/wp-content/themes/pacific-beach-homes/assets/images/backgrounds/backgroundInternal.jpg);height:200px;position:relative;width:100%;z-index:3}.banner-text,.banner-text-internal{position:absolute;width:100%;left:0;font-family:var(--headerFont);font-weight:400;color:var(--white);text-align:center}.banner-text{font-size:4.7rem;margin:auto;line-height:1;top:23%;transition:font-size .3s ease-in-out,opacity 1s ease-in-out;opacity:0}.banner-text-internal{top:67%;font-size:11rem;transform:translate3d(0,-50%,0);opacity:.2;text-shadow:0 0 5px var(--black);overflow:hidden}.mobileMenu{height:calc(var(--vh,1vh) * 100);width:0;position:fixed;z-index:19;left:0;top:0;justify-content:space-between;background-color:#000;background-color:rgba(0,120,163,.9);overflow:hidden;transition:.8s}a.menu-icon{position:relative;flex:1 1 auto;justify-content:center;height:80px;cursor:pointer;z-index:20}.hamburgerContainer{display:flex;flex-direction:column;align-items:center;justify-content:center;height:34px;margin:0}.hamburger,.hamburger:after,.hamburger:before{background:#fff;height:3px;box-shadow:0 2px 5px rgba(0,0,0,.2)}.hamburger{position:relative;width:40px;transition:.8s}.hamburger:after,.hamburger:before{content:'';position:absolute;right:0;left:0;margin:auto;width:30px;transition:.8s,transform .8s;z-index:8}.hamburger:before{top:-10px}.hamburger:after{top:10px}.hamburgertext{font-family:var(--headerFont);font-size:1.75rem;font-weight:400;color:var(--white);transition:.8s;text-align:center}@media (min-width:769px){a.menu-icon{display:none}#masthead.is-visible{opacity:1;transform:translate(0,0);-webkit-transform:translate(0,0);transition:transform .3s,background .3s,color .3s,opacity 0 .3s;-webkit-transition:-webkit-transform .3s,background .3s,color .3s}.site-branding{display:flex;margin:10px 10px 10px 15px}.main-navigation{margin:10px 15px 10px 10px;justify-content:flex-end}a.mobile-button{display:none}.menu-main-container{display:flex;flex-direction:row;align-items:center;justify-content:flex-end}.menu-main-container ul{display:flex;justify-content:space-between}.menu-main-container li{font-size:2.15rem;padding:0 1.4rem;font-family:var(--headerFont)}.menu-main-container a{text-decoration:none;color:var(--white);font-weight:700;letter-spacing:.5px}.banner-text{font-size:7.2rem}.container-banner-text{top:45%}.internal-banner{min-height:25vh}.banner-text-internal{top:65%;font-size:15rem}}@media (min-width:1025px){.menu-main-container li{font-size:2.7rem}.banner-text{font-size:8.1rem}}@media (min-width:1350px){.internal-banner{min-height:30vh}.banner-text-internal{top:65%;font-size:20rem}}@media (min-width:1600px){.nav-container{max-width:1600px;margin:auto}}</style>
+    <?php
+}
+add_action('wp_head', 'hook_css');
+
+
+
+//preload style sheets to defer stylesheets - doesn't work for firefox
+
+// function add_rel_preload($html, $handle, $href, $media) {
+    
+//     if (is_admin())
+//         return $html;
+
+//      $html = <<<EOT
+// <link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
+// EOT;
+//     return $html;
+// }
+// add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
